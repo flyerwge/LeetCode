313d1aa7a10652df91795cc6454232b3d28758cb
flyergw <flyerwge@163.com>
2021-06-29 14:29:38 +0800

    2021-06-29

---
+++ 121.买卖股票的最佳时机.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=java
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+}
+// @lc code=end
---
+++ 239.滑动窗口最大值.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=239 lang=java
+ *
+ * [239] 滑动窗口最大值
+ */
+
+// @lc code=start
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] ans = new int[nums.length - (k - 1)];
+
+        int max = nums[0];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
